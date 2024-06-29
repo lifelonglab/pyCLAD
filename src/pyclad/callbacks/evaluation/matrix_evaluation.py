076afb6438e61f0c6af02261248d61ce7285cb55
Kamil Faber <kfaber@agh.edu.pl>
2024-06-29 23:59:00 +0200

    Add MatrixMetricEvaluationCallback which allows to evaluate continual metrics based on matrix of base metric values

---
+++ src/pyclad/callbacks/evaluation/matrix_evaluation.py
@@ -0,0 +1,53 @@
+from collections import defaultdict
+from typing import Dict, List, Iterable, Any
+
+import numpy as np
+
+from pyclad.callbacks.callback import Callback
+from pyclad.data.concept import Concept
+from pyclad.metrics.base.base_metric import BaseMetric
+from pyclad.metrics.continual.concepts_metric import ConceptLevelMatrixMetric, ConceptLevelMatrix
+from pyclad.output.output_writer import InfoProvider
+
+
+class MatrixMetricEvaluationCallback(Callback, InfoProvider):
+    def __init__(self, base_metric: BaseMetric, metrics: Iterable[ConceptLevelMatrixMetric]):
+        self._base_metric: BaseMetric = base_metric
+        self._metric_matrix: Dict[str, Dict[str, float]] = defaultdict(dict)
+        self._learned_concepts: List[str] = []
+        self._metrics = metrics
+
+    def after_training(self, learned_concept: Concept):
+        self._learned_concepts.append(learned_concept.name)
+
+    def after_evaluation(self, evaluated_concept: Concept, y_true: np.ndarray, y_pred: np.ndarray,
+                         anomaly_scores: np.ndarray, *args, **kwargs):
+        assert evaluated_concept.name not in self._metric_matrix[
+            self._learned_concepts[-1]], "The same concept should not be evaluated twice after the same learned concept"
+
+        metric_value = self._base_metric.compute(anomaly_scores=anomaly_scores, y_true=y_true, y_pred=y_pred)
+        self._metric_matrix[self._learned_concepts[-1]][evaluated_concept.name] = metric_value
+
+    def info(self) -> Dict[str, Any]:
+
+        concept_level_matrix = self._transform_to_ordered_matrix(self._metric_matrix, self._learned_concepts)
+        lifelong_learning_metrics = {m.name(): m.compute(concept_level_matrix) for m in self._metrics}
+
+        return {
+            f'matrixMetricEvaluationCallback_{self._base_metric.name()}': {
+                self._base_metric.name(): {'metrics': lifelong_learning_metrics,
+                                           'concepts_order': self._learned_concepts,
+                                           'matrix': self._metric_matrix}}
+        }
+
+    @staticmethod
+    def _transform_to_ordered_matrix(metric_matrix: Dict[str, Dict[str, float]],
+                                     concepts_order: List[str]) -> ConceptLevelMatrix:
+        values = []
+
+        for learned_concept in concepts_order:
+            values.append([])
+            for evaluated_concept in concepts_order:
+                values[-1].append(metric_matrix[learned_concept][evaluated_concept])
+
+        return values
