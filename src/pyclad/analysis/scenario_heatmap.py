a95392cb57856ca0b226076064dc0768e8a40e04
Kamil Faber <kfaber@agh.edu.pl>
2024-08-13 15:38:58 +0200

    Update example and add additional options for heatmap

--- src/pyclad/analysis/scenario_heatmap.py
+++ src/pyclad/analysis/scenario_heatmap.py
@@ -2,22 +2,33 @@ import pathlib
 from typing import Dict, List
 
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import seaborn as sns
 from matplotlib.axes import Axes
 
 
+def _create_upper_diagonal_mask(tasks_no: int) -> np.array:
+    mask = np.zeros((tasks_no, tasks_no))
+    for i in range(tasks_no):
+        for j in range(tasks_no):
+            if j > i:
+                mask[i, j] = True
+    return mask
+
+
 def plot_metric_heatmap(
-    matrix: Dict,
-    concepts_order: List[str],
-    output_path: pathlib.Path = None,
-    names_mapping: Dict[str, str] = None,
-    xlabel: str = "Evaluating on concept",
-    ylabel: str = "After learning concept",
-    title: str = "Performance Heatmap",
-    annotate: bool = False,
-    color_palette: str = "plasma",
-    figsize: tuple = (6, 5),
+        matrix: Dict,
+        concepts_order: List[str],
+        output_path: pathlib.Path = None,
+        names_mapping: Dict[str, str] = None,
+        xlabel: str = "Evaluating on concept",
+        ylabel: str = "After learning concept",
+        title: str = "Performance Heatmap",
+        annotate: bool = False,
+        color_palette: str = "plasma",
+        figsize: tuple = (6, 5),
+        ignore_upper_diagonal: bool = False,
 ):
     sns.set_theme(style="darkgrid")
     sns.set(rc={"figure.figsize": figsize})
@@ -38,13 +49,16 @@ def plot_metric_heatmap(
     df = pd.DataFrame(data, columns=["learned_concept", "evaluated_concept", "metric_value"])
     df = df.pivot(index="learned_concept", columns="evaluated_concept", values="metric_value")
     p: Axes = sns.heatmap(
-        df, vmin=0, vmax=1, center=0.5, cmap=sns.color_palette(color_palette, as_cmap=True), annot=annotate
+        df, vmin=0, vmax=1, center=0.5, cmap=sns.color_palette(color_palette, as_cmap=True),
+        annot=annotate,
+        mask=None if ignore_upper_diagonal else _create_upper_diagonal_mask(len(concepts_order))
     )
     p.set_xlabel(xlabel)
     p.set_ylabel(ylabel)
     p.set_title(title)
 
     if output_path is not None:
+        plt.tight_layout()
         plt.savefig(output_path)
 
     return p
